6aa5332bb54b53df8e41c280d07efe52735c4c79
Vaibhav <vaibhavkarande2552@gmail.com>
2021-02-20 15:33:54 +0530

    New upadated code

---
+++ assets/script.js
@@ -0,0 +1,65 @@
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("navbar");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+// ----------------------------------------------------------------------------------
+
+$('a[href^="#"]').on('click',function (e) {
+    // e.preventDefault();
+
+    var target = this.hash,
+    $target = $(target);
+
+   $('html, body').stop().animate({
+     'scrollTop': $target.offset().top-70
+    }, 900, 'swing', function () {
+     window.location.hash = target;
+    });
+
+    $('ul li').click(function(){
+        $('li').removeClass("active");
+        $(this).addClass("active");
+    });
+});
+
+// -------------------- mob gallery --------------------
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
